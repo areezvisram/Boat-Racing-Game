4c2090c0e521b24c5751112347b1cf36775a49ab
2547technowizz <neil_edward_lobo@hotmail.com>
2021-10-29 00:34:59 -0400

    start of mesh class

--- classes/mesh.h
+++ classes/mesh.h
@@ -1,10 +1,23 @@
 #ifndef MESH_H
 #define MESH_H
 
+#include "mathLib3D.h"
+#include "vector"
+
+struct Face
+{
+    int vertexIndex;
+    int normalIndex;
+};
+
 class Mesh
 {
+private:
+    std::vector<Point3D> VBO;   //vertex buffer object
+    std::vector<Point3D> NBO;   //normal buffer object
+    std::vector<Face> faces;    //faces
 public:
-    // Mesh()
+    Mesh();
 };
 
 #endif
